d0149b94ed891eafd56497b0068c0fb671182456
Jonas Correia <jonas149.correia@gmail.com>
2025-08-30 21:36:17 -0300

    vamos lá

--- Aprendizado/TestProject1/Script/Aprendendo.js.bak
+++
@@ -1,8 +0,0 @@
-﻿function LogToResult(){
-  // comentarios 
-  // TODO 1 -cteste -oJonas2 : aprendo a mexer
-  // DONE 2 -cRevisaã -oJonas  : aprendido
-  Log.Message("Oi");
-  Log.Message("Oi novamente");
-  
-}
--- Aprendizado/TestProject1/Script/BlocoNotas.js
+++
@@ -1,7 +0,0 @@
-﻿function  teste (){
-  // Escreve um frase no bloco de notas apenas se o bloco de notas estiver aberto
-  //Sys.Process("Notepad").Window("Notepad", "* Sem título - Bloco de notas", 1).Window("NotepadTextBox", "", 1).Window("RichEditD2DPT", "", 1).wText = "Escrevendo no bloco de notas"
-  
-  NameMapping.Sys.ProssNotepad.MainWindNotepad.BoxNotepad.EditNotepad.wText = "Realizando a escrita no notepad vazio pelo Name Mapping"
-  
-}
